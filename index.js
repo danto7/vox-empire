a830258d26a1b7fad974b1bd19b4e3e65d66f4aa
Daniel Jensen <jensen.daniel97@gmail.com>
2015-02-22 16:33:50 +0100

    added basic threejs setup

---
+++ index.js
@@ -0,0 +1,15 @@
+//Global THREE JS variables
+var scene, renderer, camera;
+
+window.onload = function(){
+	init()
+}
+
+var init = function(){
+	scene = new THREE.Scene()
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+	renderer = new THREE.WebGLRenderer()
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	document.body.appendChild(renderer.domElement)
+}
