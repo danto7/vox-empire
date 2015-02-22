1fec42f08a822f00a245ef06fdb1725d24c6a3ad
Daniel Jensen <jensen.daniel97@gmail.com>
2015-02-22 16:40:16 +0100

    added cube

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 //Global THREE JS variables
 var scene, renderer, camera;
 
+//Some Debug setup
+var cube;
+
 window.onload = function(){
 	init()
+	render()
 }
 
 var init = function(){
@@ -12,4 +16,21 @@ var init = function(){
 	renderer = new THREE.WebGLRenderer()
 	renderer.setSize(window.innerWidth, window.innerHeight)
 	document.body.appendChild(renderer.domElement)
+
+	var geometry = new THREE.BoxGeometry(1, 1, 1)
+	var material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+	cube = new THREE.Mesh(geometry, material)
+	scene.add(cube)
+
+	camera.position.z = 5
+}
+
+var animate = function(){
+
+}
+
+var render = function(){
+	requestAnimationFrame(render);
+	animate()
+	renderer.render(scene, camera);
 }
